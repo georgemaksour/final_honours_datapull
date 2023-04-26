b14218e70cb0ffe1461152db470c6122fcc491b8
George Maksour <gmaksour@yahoo.com>
2023-04-27 00:19:47 +1000

    Fixing pathing of files

--- models.py
+++
@@ -1,7 +0,0 @@
-from config import *
-
-def commonwealth_games(data, statistics) -> int:
-    """Used historical data and recent performance to calculate probabilistic outcomes,
-    deriving probability distributions for total medals and futures markets.
-    Applied hypothesis testing for subcategories of games to improve pricing"""
-    return
